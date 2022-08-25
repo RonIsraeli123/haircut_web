2dc4ced74183c3bd06e566ecf408f2461c070cfc
Ron <stamdvarim1000@gmail.com>
2022-08-25 13:31:08 +0300

    add call icon

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MdEmail } from 'react-icons/md';
 import { ImWhatsapp } from 'react-icons/im';
+import { BsFillTelephoneFill } from 'react-icons/bs';
 import { MAIL_ADDRESS, PHONE_NUMBER } from '../config/homePage/contact';
 
 function SocialMedia() {
@@ -9,12 +10,12 @@ function SocialMedia() {
       <ul className='home-about-social-links'>
         <li className='social-icons'>
           <a
-            href={`mailto:${MAIL_ADDRESS}`}
+            href={`tel:${PHONE_NUMBER}`}
             target='_blank'
             rel='noreferrer'
             className='icon-colour home-social-icons'
           >
-            <MdEmail className='social-icon' />
+            <BsFillTelephoneFill className='social-icon' />
           </a>
         </li>
       </ul>
@@ -28,6 +29,18 @@ function SocialMedia() {
           >
             {' '}
             <ImWhatsapp className='social-icon' />
+          </a>
+        </li>
+      </ul>
+      <ul className='home-about-social-links'>
+        <li className='social-icons'>
+          <a
+            href={`mailto:${MAIL_ADDRESS}`}
+            target='_blank'
+            rel='noreferrer'
+            className='icon-colour home-social-icons'
+          >
+            <MdEmail className='social-icon' />
           </a>
         </li>
       </ul>
